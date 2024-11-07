b23aca7bc477a62818ada23bcc857c221fe8dea4
Gustavo Mandu Ferreira Matori <gmandu10@gmail.com>
2024-11-07 14:57:43 -0300

    first commit

---
+++ public/js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3p3zIZbir8B2DJJBLnD5BnemTvMkBynU",
+    authDomain: "musiclearning182.firebaseapp.com",
+    projectId: "musiclearning182",
+    storageBucket: "musiclearning182.appspot.com",
+    messagingSenderId: "587881417309",
+    appId: "1:587881417309:web:1898c962c1c2e85bd29bb4",
+    measurementId: "G-6QSM7F4718"
+  };
